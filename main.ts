a94602e28607eabbea4f6f0b99a315b80e3f1097
habonzak <91627828+habonzak@users.noreply.github.com>
2022-02-09 12:38:53 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -16,156 +16,6 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Item, function (sprite, otherSpr
         hasBurger = true
     }
 })
-function batHeroFly () {
-    mainFlyLeft = animation.createAnimation(ActionKind.Walking, 100)
-    animation.attachAnimation(heroBat, mainFlyLeft)
-    mainFlyLeft.addAnimationFrame(img`
-        . . f f f . . . . . . . . f f f 
-        . f f c c . . . . . . f c b b c 
-        f f c c . . . . . . f c b b c . 
-        f c f c . . . . . . f b c c c . 
-        f f f c c . c c . f c b b c c . 
-        f f c 3 c c 3 c c f b c b b c . 
-        f f b 3 b c 3 b c f b c c b c . 
-        . c b b b b b b c b b c c c . . 
-        . c 1 b b b 1 b b c c c c . . . 
-        c b b b b b b b b b c c . . . . 
-        c b c b b b c b b b b f . . . . 
-        f b 1 f f f 1 b b b b f c . . . 
-        f b b b b b b b b b b f c c . . 
-        . f b b b b b b b b c f . . . . 
-        . . f b b b b b b c f . . . . . 
-        . . . f f f f f f f . . . . . . 
-        `)
-    mainFlyLeft.addAnimationFrame(img`
-        . . f f f . . . . . . . . . . . 
-        f f f c c . . . . . . . . f f f 
-        f f c c . . c c . . . f c b b c 
-        f f c 3 c c 3 c c f f b b b c . 
-        f f b 3 b c 3 b c f b b c c c . 
-        . c b b b b b b c f b c b c c . 
-        . c b b b b b b c b b c b b c . 
-        c b 1 b b b 1 b b b c c c b c . 
-        c b b b b b b b b c c c c c . . 
-        f b c b b b c b b b b f c . . . 
-        f b 1 f f f 1 b b b b f c c . . 
-        . f b b b b b b b b c f . . . . 
-        . . f b b b b b b c f . . . . . 
-        . . . f f f f f f f . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `)
-    mainFlyLeft.addAnimationFrame(img`
-        . . . . . . . . . . . . . . . . 
-        . . c c . . c c . . . . . . . . 
-        . . c 3 c c 3 c c c . . . . . . 
-        . c b 3 b c 3 b c c c . . . . . 
-        . c b b b b b b b b f f . . . . 
-        c c b b b b b b b b f f . . . . 
-        c b 1 b b b 1 b b c f f f . . . 
-        c b b b b b b b b f f f f . . . 
-        f b c b b b c b c c b b b . . . 
-        f b 1 f f f 1 b f c c c c . . . 
-        . f b b b b b b f b b c c . . . 
-        c c f b b b b b c c b b c . . . 
-        c c c f f f f f f c c b b c . . 
-        . c c c . . . . . . c c c c c . 
-        . . c c c . . . . . . . c c c c 
-        . . . . . . . . . . . . . . . . 
-        `)
-    mainFlyLeft.addAnimationFrame(img`
-        . f f f . . . . . . . . f f f . 
-        f f c . . . . . . . f c b b c . 
-        f c c . . . . . . f c b b c . . 
-        c f . . . . . . . f b c c c . . 
-        c f f . . . . . f f b b c c . . 
-        f f f c c . c c f b c b b c . . 
-        f f f c c c c c f b c c b c . . 
-        . f c 3 c c 3 b c b c c c . . . 
-        . c b 3 b c 3 b b c c c c . . . 
-        c c b b b b b b b b c c . . . . 
-        c b 1 b b b 1 b b b b f c . . . 
-        f b b b b b b b b b b f c c . . 
-        f b c b b b c b b b b f . . . . 
-        . f 1 f f f 1 b b b c f . . . . 
-        . . f b b b b b b c f . . . . . 
-        . . . f f f f f f f . . . . . . 
-        `)
-    MainFlyRight = animation.createAnimation(ActionKind.Walking, 100)
-    animation.attachAnimation(heroBat, MainFlyRight)
-    MainFlyRight.addAnimationFrame(img`
-        f f f . . . . . . . . f f f . . 
-        c b b c f . . . . . . c c f f . 
-        . c b b c f . . . . . . c c f f 
-        . c c c b f . . . . . . c f c f 
-        . c c b b c f . c c . c c f f f 
-        . c b b c b f c c 3 c c 3 c f f 
-        . c b c c b f c b 3 c b 3 b f f 
-        . . c c c b b c b b b b b b c . 
-        . . . c c c c b b 1 b b b 1 c . 
-        . . . . c c b b b b b b b b b c 
-        . . . . f b b b b c b b b c b c 
-        . . . c f b b b b 1 f f f 1 b f 
-        . . c c f b b b b b b b b b b f 
-        . . . . f c b b b b b b b b f . 
-        . . . . . f c b b b b b b f . . 
-        . . . . . . f f f f f f f . . . 
-        `)
-    MainFlyRight.addAnimationFrame(img`
-        . . . . . . . . . . . f f f . . 
-        f f f . . . . . . . . c c f f f 
-        c b b c f . . . c c . . c c f f 
-        . c b b b f f c c 3 c c 3 c f f 
-        . c c c b b f c b 3 c b 3 b f f 
-        . c c b c b f c b b b b b b c . 
-        . c b b c b b c b b b b b b c . 
-        . c b c c c b b b 1 b b b 1 b c 
-        . . c c c c c b b b b b b b b c 
-        . . . c f b b b b c b b b c b f 
-        . . c c f b b b b 1 f f f 1 b f 
-        . . . . f c b b b b b b b b f . 
-        . . . . . f c b b b b b b f . . 
-        . . . . . . f f f f f f f . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `)
-    MainFlyRight.addAnimationFrame(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . c c . . c c . . 
-        . . . . . . c c c 3 c c 3 c . . 
-        . . . . . c c c b 3 c b 3 b c . 
-        . . . . f f b b b b b b b b c . 
-        . . . . f f b b b b b b b b c c 
-        . . . f f f c b b 1 b b b 1 b c 
-        . . . f f f f b b b b b b b b c 
-        . . . b b b c c b c b b b c b f 
-        . . . c c c c f b 1 f f f 1 b f 
-        . . . c c b b f b b b b b b f . 
-        . . . c b b c c b b b b b f c c 
-        . . c b b c c f f f f f f c c c 
-        . c c c c c . . . . . . c c c . 
-        c c c c . . . . . . . c c c . . 
-        . . . . . . . . . . . . . . . . 
-        `)
-    MainFlyRight.addAnimationFrame(img`
-        . f f f . . . . . . . . f f f . 
-        . c b b c f . . . . . . . c f f 
-        . . c b b c f . . . . . . c c f 
-        . . c c c b f . . . . . . . f c 
-        . . c c b b f f . . . . . f f c 
-        . . c b b c b f c c . c c f f f 
-        . . c b c c b f c c c c c f f f 
-        . . . c c c b c b 3 c c 3 c f . 
-        . . . c c c c b b 3 c b 3 b c . 
-        . . . . c c b b b b b b b b c c 
-        . . . c f b b b b 1 b b b 1 b c 
-        . . c c f b b b b b b b b b b f 
-        . . . . f b b b b c b b b c b f 
-        . . . . f c b b b 1 f f f 1 f . 
-        . . . . . f c b b b b b b f . . 
-        . . . . . . f f f f f f f . . . 
-        `)
-}
 function initilizeBackgound () {
     tiles.setTilemap(tilemap`level1`)
     scene.setBackgroundImage(img`
@@ -291,18 +141,13 @@ function initilizeBackgound () {
         ................................................................................................................................................................
         `)
 }
-let facingRight = false
-let facingLeft = false
 let questsDone = 0
 let ghostDone = false
-let MainFlyRight: animation.Animation = null
-let mainFlyLeft: animation.Animation = null
 let hasBurger = false
 let currentItem = 0
 let burger: Sprite = null
-let heroBat: Sprite = null
 initilizeBackgound()
-heroBat = sprites.create(img`
+let heroBat = sprites.create(img`
     . . f f f . . . . . . . . f f f 
     . f f c c . . . . . . f c b b c 
     f f c c . . . . . . f c b b c . 
@@ -386,12 +231,5 @@ game.onUpdate(function () {
         } else {
             ghostFriend.sayText("What is this? :(")
         }
-    }
-})
-game.onUpdate(function () {
-    if (heroBat.vx < 0) {
-        facingLeft = true
-    } else if (heroBat.vx > 0) {
-        facingRight = true
     }
 })
