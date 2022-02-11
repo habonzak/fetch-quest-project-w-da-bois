034ed78c5d23ba0963959d37de3376d5e3213bd5
farmerflynn <91627834+farmerflynn@users.noreply.github.com>
2022-02-11 13:35:24 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+enum ActionKind {
+    Walking,
+    Idle,
+    Jumping
+}
 namespace SpriteKind {
     export const Friend = SpriteKind.create()
     export const Item = SpriteKind.create()
@@ -327,7 +332,6 @@ function talkFriend (who: Sprite, thing: boolean, quest: boolean, chat: boolean)
     }
 }
 let facingRight = false
-let facingLeft = false
 let MainFlyRight: animation.Animation = null
 let mainFlyLeft: animation.Animation = null
 let hasCake = false
@@ -361,11 +365,6 @@ let introText = [
 ]
 for (let value of introText) {
     game.showLongText(value, DialogLayout.Center)
-}
-class ActionKind {
-    static Walking = 0
-    static Idle = 1
-    static Jumping = 2
 }
 // initialize sprites
 heroBat = sprites.create(img`
@@ -479,6 +478,80 @@ rejectText = [
 "Gross :(",
 "I don't like this."
 ]
+let animBat = [
+img`
+    . . f f f . . . . . . . . . . . 
+    f f f c c . . . . . . . . f f f 
+    f f c c . . c c . . . f c b b c 
+    f f c 3 c c 3 c c f f b b b c . 
+    f f b 3 b c 3 b c f b b c c c . 
+    . c b b b b b b c f b c b c c . 
+    . c b b b b b b c b b c b b c . 
+    c b 1 b b b 1 b b b c c c b c . 
+    c b b b b b b b b c c c c c . . 
+    f b c b b b c b b b b f c . . . 
+    f b 1 f f f 1 b b b b f c c . . 
+    . f b b b b b b b b c f . . . . 
+    . . f b b b b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . c c . . c c . . . . . . . . 
+    . . c 3 c c 3 c c c . . . . . . 
+    . c b 3 b c 3 b c c c . . . . . 
+    . c b b b b b b b b f f . . . . 
+    c c b b b b b b b b f f . . . . 
+    c b 1 b b b 1 b b c f f f . . . 
+    c b b b b b b b b f f f f . . . 
+    f b c b b b c b c c b b b . . . 
+    f b 1 f f f 1 b f c c c c . . . 
+    . f b b b b b b f b b c c . . . 
+    c c f b b b b b c c b b c . . . 
+    c c c f f f f f f c c b b c . . 
+    . c c c . . . . . . c c c c c . 
+    . . c c c . . . . . . . c c c c 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . f f f . . . . . . . . f f f . 
+    f f c . . . . . . . f c b b c . 
+    f c c . . . . . . f c b b c . . 
+    c f . . . . . . . f b c c c . . 
+    c f f . . . . . f f b b c c . . 
+    f f f c c . c c f b c b b c . . 
+    f f f c c c c c f b c c b c . . 
+    . f c 3 c c 3 b c b c c c . . . 
+    . c b 3 b c 3 b b c c c c . . . 
+    c c b b b b b b b b c c . . . . 
+    c b 1 b b b 1 b b b b f c . . . 
+    f b b b b b b b b b b f c c . . 
+    f b c b b b c b b b b f . . . . 
+    . f 1 f f f 1 b b b c f . . . . 
+    . . f b b b b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    `,
+img`
+    . f f f . . . . . . . . f f f . 
+    f f c . . . . . . . f c b b c . 
+    f c c . . . . . . f c b b c . . 
+    c f . . . . . . . f b c c c . . 
+    c f f . . . . . f f b b c c . . 
+    f f f c c . c c f b c b b c . . 
+    f f f c c c c c f b c c b c . . 
+    . f c 3 c c 3 b c b c c c . . . 
+    . c b 3 b c 3 b b c c c c . . . 
+    c c b b b b b b b b c c . . . . 
+    c b 1 b b b 1 b b b b f c . . . 
+    f b b b b b b b b b b f c c . . 
+    f b c b b b c b b b b f . . . . 
+    . f 1 f f f 1 b b b c f . . . . 
+    . . f b b b b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    `
+]
 // calls functions
 game.onUpdate(function () {
     talkFriend(ghostFriend, hasBurger, ghostDone, talkGhost)
@@ -486,9 +559,86 @@ game.onUpdate(function () {
     talkFriend(snakeFriend, hasCake, snakeDone, talkSnake)
 })
 game.onUpdate(function () {
-    if (heroBat.vx < 0) {
-        facingLeft = true
-    } else if (heroBat.vx > 0) {
-        facingRight = true
+    if (heroBat.vx > 0) {
+        if (heroBat.vy > 0) {
+            heroBat.setImage(img`
+                . f f f . . . . . . . . f f f . 
+                . c b b c f . . . . . . . c f f 
+                . . c b b c f . . . . . . c c f 
+                . . c c c b f . . . . . . . f c 
+                . . c c b b f f . . . . . f f c 
+                . . c b b c b f c c . c c f f f 
+                . . c b c c b f c c c c c f f f 
+                . . . c c c b c b 3 c c 3 c f . 
+                . . . c c c c b b 3 c b 3 b c . 
+                . . . . c c b b b b b b b b c c 
+                . . . c f b b b b 1 b b b 1 b c 
+                . . c c f b b b b b b b b b b f 
+                . . . . f b b b b c b b b c b f 
+                . . . . f c b b b 1 f f f 1 f . 
+                . . . . . f c b b b b b b f . . 
+                . . . . . . f f f f f f f . . . 
+                `)
+        } else {
+            heroBat.setImage(img`
+                f f f . . . . . . . . f f f . . 
+                c b b c f . . . . . . c c f f . 
+                . c b b c f . . . . . . c c f f 
+                . c c c b f . . . . . . c f c f 
+                . c c b b c f . c c . c c f f f 
+                . c b b c b f c c 3 c c 3 c f f 
+                . c b c c b f c b 3 c b 3 b f f 
+                . . c c c b b c b b b b b b c . 
+                . . . c c c c b b 1 b b b 1 c . 
+                . . . . c c b b b b b b b b b c 
+                . . . . f b b b b c b b b c b c 
+                . . . c f b b b b 1 f f f 1 b f 
+                . . c c f b b b b b b b b b b f 
+                . . . . f c b b b b b b b b f . 
+                . . . . . f c b b b b b b f . . 
+                . . . . . . f f f f f f f . . . 
+                `)
+        }
+    } else if (heroBat.vx < 0) {
+        if (heroBat.vy < 0) {
+            heroBat.setImage(img`
+                . f f f . . . . . . . . f f f . 
+                f f c . . . . . . . f c b b c . 
+                f c c . . . . . . f c b b c . . 
+                c f . . . . . . . f b c c c . . 
+                c f f . . . . . f f b b c c . . 
+                f f f c c . c c f b c b b c . . 
+                f f f c c c c c f b c c b c . . 
+                . f c 3 c c 3 b c b c c c . . . 
+                . c b 3 b c 3 b b c c c c . . . 
+                c c b b b b b b b b c c . . . . 
+                c b 1 b b b 1 b b b b f c . . . 
+                f b b b b b b b b b b f c c . . 
+                f b c b b b c b b b b f . . . . 
+                . f 1 f f f 1 b b b c f . . . . 
+                . . f b b b b b b c f . . . . . 
+                . . . f f f f f f f . . . . . . 
+                `)
+        } else {
+            heroBat.setImage(img`
+                . . f f f . . . . . . . . f f f 
+                . f f c c . . . . . . f c b b c 
+                f f c c . . . . . . f c b b c . 
+                f c f c . . . . . . f b c c c . 
+                f f f c c . c c . f c b b c c . 
+                f f c 3 c c 3 c c f b c b b c . 
+                f f b 3 b c 3 b c f b c c b c . 
+                . c b b b b b b c b b c c c . . 
+                . c 1 b b b 1 b b c c c c . . . 
+                c b b b b b b b b b c c . . . . 
+                c b c b b b c b b b b f . . . . 
+                f b 1 f f f 1 b b b b f c . . . 
+                f b b b b b b b b b b f c c . . 
+                . f b b b b b b b b c f . . . . 
+                . . f b b b b b b c f . . . . . 
+                . . . f f f f f f f . . . . . . 
+                `)
+            facingRight = false
+        }
     }
 })
