d9bc8b3f440ba2d2325e91f121ea6c568595a325
habonzak <91627828+habonzak@users.noreply.github.com>
2022-02-17 13:14:39 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -60,25 +60,6 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     }
 })
 function setSprites () {
-    // initialize sprites
-    heroBat = sprites.create(img`
-        . . f f f . . . . . . . . f f f 
-        . f f c c . . . . . . f c b b c 
-        f f c c . . . . . . f c b b c . 
-        f c f c . . . . . . f b c c c . 
-        f f f c c . c c . f c b b c c . 
-        f f c 3 c c 3 c c f b c b b c . 
-        f f b 3 b c 3 b c f b c c b c . 
-        . c b b b b b b c b b c c c . . 
-        . c 1 b b b 1 b b c c c c . . . 
-        c b b b b b b b b b c c . . . . 
-        c b c b b b c b b b b f . . . . 
-        f b 1 f f f 1 b b b b f c . . . 
-        f b b b b b b b b b b f c c . . 
-        . f b b b b b b b b c f . . . . 
-        . . f b b b b b b c f . . . . . 
-        . . . f f f f f f f . . . . . . 
-        `, SpriteKind.Player)
     controller.moveSprite(heroBat)
     scene.cameraFollowSprite(heroBat)
     ghostFriend = sprites.create(img`
@@ -776,7 +757,6 @@ let talkGhost = false
 let snakeFriend: Sprite = null
 let duckFriend: Sprite = null
 let ghostFriend: Sprite = null
-let heroBat: Sprite = null
 let questsDone = 0
 let snakeDone = false
 let duckDone = false
@@ -812,11 +792,31 @@ let cake: Sprite = null
 let pizza: Sprite = null
 let burger: Sprite = null
 let currentItem = false
+let heroBat: Sprite = null
 let introText: string[] = []
 setArrays()
 for (let value of introText) {
     game.showLongText(value, DialogLayout.Center)
 }
+// initialize sprites
+heroBat = sprites.create(img`
+    . . f f f . . . . . . . . f f f 
+    . f f c c . . . . . . f c b b c 
+    f f c c . . . . . . f c b b c . 
+    f c f c . . . . . . f b c c c . 
+    f f f c c . c c . f c b b c c . 
+    f f c 3 c c 3 c c f b c b b c . 
+    f f b 3 b c 3 b c f b c c b c . 
+    . c b b b b b b c b b c c c . . 
+    . c 1 b b b 1 b b c c c c . . . 
+    c b b b b b b b b b c c . . . . 
+    c b c b b b c b b b b f . . . . 
+    f b 1 f f f 1 b b b b f c . . . 
+    f b b b b b b b b b b f c c . . 
+    . f b b b b b b b b c f . . . . 
+    . . f b b b b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    `, SpriteKind.Player)
 setBackground()
 setSprites()
 setPresets()
